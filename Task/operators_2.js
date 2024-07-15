3ccf8c99364118917e8e035aa8dd44bd8293933a
1Fin_work <radhesh.kumre@1finance.co.in>
2024-07-15 11:30:51 +0530

    Radhesh: Add DAY 1 Task

---
+++ Task/operators_2.js
@@ -0,0 +1,22 @@
+//Activity 1
+const num1 = 45
+const num2 = 20
+
+console.log(num1+num2)
+console.log(num1-num2)
+console.log(num1*num2)
+console.log(num1/num2)
+console.log(num1%num2)
+
+
+//Activity 2
+let num3 = 47
+console.log(num3+=2)
+console.log(num3-=2)
+
+//Activity 3
+console.log("Greater than 20:",num1>num2)
+console.log("Less than 20:",num1<num2)
+console.log("Greaterthan or Equal to 20:", num1>=20)
+console.log("Lessthan or Equal to 20:", num1<=20)
+console.log()
---
+++ Task/variables_1.js
@@ -0,0 +1,38 @@
+//Activity 1
+var number;
+number = 10
+console.log(number)
+
+let string;
+string = 'string'
+console.log(string)
+
+
+//Activity 2
+const boolean = true
+console.log(boolean)
+
+//Activity 3
+const num = 10;
+const str = 'string';
+const value = false
+const obj = {key:'Value'}
+const arr = [1,'str',true]
+
+console.log(typeof(num))
+console.log(typeof(str))
+console.log(typeof(value))
+console.log(typeof(obj))
+console.log(typeof(arr))
+
+
+
+//Activity 4
+let thevalue = 51
+console.log(thevalue)
+
+thevalue = 'value'
+console.log(thevalue)
+
+//Activity 5
+value = true
